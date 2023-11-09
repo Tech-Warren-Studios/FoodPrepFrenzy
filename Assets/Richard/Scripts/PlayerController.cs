15630ba59fe80efcc72430abaee08ed23338a466
Richard Brent <roxas90111@gmail.com>
2023-11-09 14:10:02 -0500

    11/9/2023 2:09 PM

--- Assets/Richard/Scripts/PlayerController.cs
+++ Assets/Richard/Scripts/PlayerController.cs
@@ -2,32 +2,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f; // You can adjust this speed in the Unity Inspector
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 720f; // Degrees per second to rotate
 
     private Rigidbody rb;
     private Vector3 movement;
 
     void Start()
     {
-        // Get the Rigidbody component for physics-based movement
         rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        // Get input from the horizontal and vertical axes
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveZ = Input.GetAxisRaw("Vertical");
 
-        // Apply the input to the movement vector
         movement = new Vector3(moveX, 0, moveZ).normalized;
     }
 
     void FixedUpdate()
     {
         // Move the player's Rigidbody
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if (movement != Vector3.zero)
+        {
+            // Move the player
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+            // Rotate the player to face the direction of movement
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+            rb.rotation = Quaternion.RotateTowards(rb.rotation, toRotation, rotationSpeed * Time.fixedDeltaTime);
+        }
     }
 }
+
 
 
